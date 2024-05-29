bba361b08442561f7e2dce6b8d86792a3a716300
Onose Ogbidi <91310019+Kored28@users.noreply.github.com>
2024-05-29 19:57:42 +0100

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -10,7 +10,7 @@ const Exercises = ({exercises, bodyPart, setExercises}) => {
   const exercisesPerPage = 9
 
   const indexOfLastExercise= currentPage * exercisesPerPage
-  const indexOfFirstExercise =indexOfLastExercise - exercisesPerPage
+  const indexOfFirstExercise = indexOfLastExercise - exercisesPerPage
   const currentExercises = exercises.slice(indexOfFirstExercise, indexOfLastExercise);
 
   const paginate = (e, value) => {
